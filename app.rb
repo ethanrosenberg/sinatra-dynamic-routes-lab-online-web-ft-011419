20a53bdb99f3fcf30cf823fbdc163f9202244775
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-11 05:51:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,9 @@ class App < Sinatra::Base
       "#{params[number1].to_i - params[:number2].to_i}"
     elsif params[:operation] == "multiply"
       "#{params[number1].to_i * params[:number2].to_i}"
+    elsif  params[:operation] == "divide"
+      "#{params[number1].to_i / params[:number2].to_i}"
+
 
   end
 end
