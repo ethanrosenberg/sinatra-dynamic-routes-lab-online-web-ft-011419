ea028f88043e87b600b69b5376ee33ef54b9958e
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-11 05:45:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,4 +9,10 @@ class App < Sinatra::Base
   get '/square/:number' do
     "#{params[:number].to_i ** 2}"
   end
+
+  get '/say/:number/:phrase' do
+    params[:num].times do
+      params[:phrase]
+    end
+  end
 end
